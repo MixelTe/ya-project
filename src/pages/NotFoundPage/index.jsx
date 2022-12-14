bebe03c1bb23207615c5a9f0c057d2d70cf1ebd5
Mike <63655075+MixelTe@users.noreply.github.com>
2022-12-14 14:42:40 +0300

    clear

--- src/pages/NotFoundPage/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import styles from "./styles.module.css"
-
-export default function NotFoundPage() {
-	return <div className={styles.root}>NotFoundPage</div>
-}
--- src/pages/ProjectsPage/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import styles from "./styles.module.css"
-
-export default function ProjectsPage() {
-	return <div className={styles.root}>ProjectsPage</div>
-}
